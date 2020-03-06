657ea0cc7e656cd88b718f6ec0ca0dafbd5f1a27
play.js <git@gituser.com>
2020-03-06 19:32:51 +0100

    Finding a single object (First since no filters given)

--- index.js
+++ index.js
@@ -7,28 +7,11 @@ MongoClient.connect(url,
     if(err) throw err;
     
     var dbo = db.db("mongo_tutorial");
-    
-    var myObj = [
-    { name: 'John', address: 'Highway 71'},
-    { name: 'Peter', address: 'Lowstreet 4'},
-    { name: 'Amy', address: 'Apple st 652'},
-    { name: 'Hannah', address: 'Mountain 21'},
-    { name: 'Michael', address: 'Valley 345'},
-    { name: 'Sandy', address: 'Ocean blvd 2'},
-    { name: 'Betty', address: 'Green Grass 1'},
-    { name: 'Richard', address: 'Sky st 331'},
-    { name: 'Susan', address: 'One way 98'},
-    { name: 'Vicky', address: 'Yellow Garden 2'},
-    { name: 'Ben', address: 'Park Lane 38'},
-    { name: 'William', address: 'Central st 954'},
-    { name: 'Chuck', address: 'Main Road 989'},
-    { name: 'Viola', address: 'Sideway 1633'}
-  ];
-    
-    dbo.collection("customers").insertMany(myObj, function(err, res) {
+       
+    dbo.collection("customers").findOne({}, function(err, result) {
       if (err) throw err;
       
-      console.log("Number of documents inserted " + res.insertedCount);
+      console.log(result.name);
     })
     
     db.close;
