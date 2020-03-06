2febaf3cbe02661dfbe55f1995352ab1bcb93efa
play.js <git@gituser.com>
2020-03-06 19:29:38 +0100

    Inserting multiple objects.

--- index.js
+++ index.js
@@ -8,14 +8,27 @@ MongoClient.connect(url,
     
     var dbo = db.db("mongo_tutorial");
     
-    var myObj = { 
-      name: "Company Inc", address: "Highway 37"
-    };
+    var myObj = [
+    { name: 'John', address: 'Highway 71'},
+    { name: 'Peter', address: 'Lowstreet 4'},
+    { name: 'Amy', address: 'Apple st 652'},
+    { name: 'Hannah', address: 'Mountain 21'},
+    { name: 'Michael', address: 'Valley 345'},
+    { name: 'Sandy', address: 'Ocean blvd 2'},
+    { name: 'Betty', address: 'Green Grass 1'},
+    { name: 'Richard', address: 'Sky st 331'},
+    { name: 'Susan', address: 'One way 98'},
+    { name: 'Vicky', address: 'Yellow Garden 2'},
+    { name: 'Ben', address: 'Park Lane 38'},
+    { name: 'William', address: 'Central st 954'},
+    { name: 'Chuck', address: 'Main Road 989'},
+    { name: 'Viola', address: 'Sideway 1633'}
+  ];
     
-    dbo.collection("customers").insertOne(myObj, function(err, res) {
+    dbo.collection("customers").insertMany(myObj, function(err, res) {
       if (err) throw err;
       
-      console.log("1 document inserted");
+      console.log("Number of documents inserted " + res.insertedCount);
     })
     
     db.close;
