5f9ebc4d5228cd0835bd69c4984726512ef4babf
play.js <git@gituser.com>
2020-03-06 19:18:30 +0100

    Connecting to a collection in the database.

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var MongoClient = require("mongodb");
 
-var url = "mongodb://test:testpwd1@ds161894.mlab.com:61894/mongo_tutorial";
+var url = "mongodb://admin:admin1234@ds161894.mlab.com:61894/mongo_tutorial";
 
 MongoClient.connect(url, 
   function(err, db) {
     if(err) throw err;
+    
+    var dbo = db.db("mongo_tutorial");
+    
+    dbo.createCollection("customers", function(err, res) {
+      if (err) throw err;
+      console.log("Collection created");
+    })
+    
     console.log("Database created!");
     db.close;
   }
