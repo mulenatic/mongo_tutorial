4a78bd2df2e2967c6727f6ea52376eee1abb3142
user <user>
2020-03-07 16:23:23 +0000

    Filter the Result
    When finding documents in a collection, you can filter the result by
    using a query object.
    
    The first argument of the find() method is a query object, and is used
    to limit the search.

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ var MongoClient = require("mongodb");
 
 var url = "mongodb://admin:admin1234@ds161894.mlab.com:61894/mongo_tutorial";
 
-MongoClient.connect(url, 
-  function(err, db) {
-    if(err) throw err;
-    
-    var dbo = db.db("mongo_tutorial");
-       
-    dbo.collection("customers").find({}, { projection: { _id: 0, name: 1, address: 1 }}).toArray(function(err, result) {
-      if (err) throw err;
-      
-      console.log(result);
-    })
-    
-    db.close;
-  }
-)
+MongoClient
+    .connect(url, 
+	     function(err, db) {
+		 if(err) throw err;
+		 
+		 var dbo = db.db("mongo_tutorial");
+		 var query = { address: "Park Lane 38" };
+		 
+		 dbo.collection("customers")
+		    .find(query).toArray(function(err, result) {
+		     if (err) throw err;
+		     
+		     console.log(result);
+		 })
+		 
+		 db.close;
+	     }
+    )
